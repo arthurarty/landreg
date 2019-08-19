860296e0b51f25e341763df8a0a26d3213b3d72c
Arthur Nangai <arthurarty@users.noreply.github.com>
2019-08-19 17:38:28 +0300

    bg(auth) - Duplicate account. (#7)
    - Handle integrity error for duplicate account.
    - Add test for bug.
    
    [Finishes #167966692]

--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,22 @@
+from django.contrib.auth import authenticate
+from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from authentication.models import User
 from authentication.serializers import UserSerializer
-from django.contrib.auth import authenticate
 
 
 @api_view(['POST'])
 def sign_up(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        try:
+            serializer.save()
+        except IntegrityError as e:
+            return Response(
+                "Number already exists",
+                status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
