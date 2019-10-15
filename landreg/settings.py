0349a3c3e16a213729d3339540eb8af52a83185b
Arthur Nangai <arthurarty@users.noreply.github.com>
2019-10-16 02:10:17 +0300

    bg(heroku) - Re-configure app. (#13)
    - Set Backend URL.
    - Add BACKEND_URL to .env_example.
    - Set STATIC_ROOT.

--- landreg/settings.py
+++ landreg/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('BACKEND_URL'),
+]
 
 
 # Application definition
@@ -133,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
